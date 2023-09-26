eb1f6b49cc2a1d08c8ac31065e6cb57dbfca1444
Andrii Bilyk <bilykaim@gmail.com>
2023-09-26 19:46:20 +0300

    update 3 tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox"; // Імпорт бібліотеки
 
-console.log(galleryItems);
+import "simplelightbox/dist/simple-lightbox.min.css"; // Імпорт CSS стилів
+
+import { galleryItems } from "./gallery-items.js"; // Імпорт галереї зображень
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  const galleryUl = document.querySelector(".gallery");
+
+
+  const galleryMarkup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>
+      </li>`;
+    })
+    .join("");
+
+
+  galleryUl.insertAdjacentHTML("beforeend", galleryMarkup);
+
+
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,58 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = feedbackForm.querySelector('input[name="email"]');
+const messageTextarea = feedbackForm.querySelector('textarea[name="message"]');
+
+// Функція для зберігання стану форми в локальному сховищі
+const saveFormState = () => {
+  const formState = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+};
+
+// Функція для відображення збереженого стану форми з локального сховища
+const restoreFormState = () => {
+  const savedState = localStorage.getItem('feedback-form-state');
+  if (savedState) {
+    const formState = JSON.parse(savedState);
+    emailInput.value = formState.email;
+    messageTextarea.value = formState.message;
+  }
+};
+
+// Встановлення обробників подій для полів вводу
+emailInput.addEventListener(
+  'input',
+  throttle(() => {
+    saveFormState();
+  }, 500)
+);
+
+messageTextarea.addEventListener(
+  'input',
+  throttle(() => {
+    saveFormState();
+  }, 500)
+);
+
+// Перевірка наявності збереженого стану та відображення його
+restoreFormState();
+
+// Встановлення обробника події submit для форми
+feedbackForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Очищення локального сховища та полів форми
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageTextarea.value = '';
+
+  // Виведення даних у консоль
+  console.log({
+    email: emailInput.value,
+    message: messageTextarea.value,
+  });
+});
